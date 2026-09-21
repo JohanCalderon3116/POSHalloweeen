91c9ab0d486780a092f92c87216bcbe4612f9322
JohanCalderon3116 <yojancalderon3116@gmail.com>
2026-09-21 08:42:57 -0500

    Cambiamos logo de variables

--- src/styles/variables.jsx
+++ src/styles/variables.jsx
@@ -12,7 +12,7 @@ import { TbBrandSupabase } from "react-icons/tb";
 import { FaReact } from "react-icons/fa";
 import { BsArrowDown, BsArrowUpShort } from "react-icons/bs";
 import { FaBalanceScale } from "react-icons/fa";
-import logo from "../../public/SoftCreate POS logo.png";
+import logo from "../../public/SoftCreatePOSHalloween.png";
 import { FcGoogle } from "react-icons/fc";
 import { RiCloseLine } from "react-icons/ri";
 import { BiSave } from "react-icons/bi";
